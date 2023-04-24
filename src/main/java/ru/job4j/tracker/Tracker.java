851941c56dd6fd0bf59e0aaf4a904553bfa66dc2
Andrey <weselov@list.ru>
2023-04-24 21:06:48 +0300

    Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.tracker;
 
 public class Tracker {
     private final Item[] items = new Item[100];
--- src/test/java/ru/job4j/TrackerTest.java
+++ src/test/java/ru/job4j/TrackerTest.java
@@ -1,6 +1,8 @@
 package ru.job4j;
 
 import org.junit.Test;
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.Tracker;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
