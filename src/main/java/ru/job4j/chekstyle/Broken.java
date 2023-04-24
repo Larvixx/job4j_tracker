d212418541f761553cdd51ebfd3f135ff083101d
Andrey <weselov@list.ru>
2023-04-24 20:54:17 +0300

    Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/chekstyle/Broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.chekstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    private final int size = 10;
+
+    public String surname;
+
+    String name;
+
+    void echo() { }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+    Broken() { }
+
+}
