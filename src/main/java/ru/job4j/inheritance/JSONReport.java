bcb38fdd01f32cba8863e80aa65893e1e291cde2
Andrey <weselov@list.ru>
2023-05-08 20:34:08 +0300

    4. Переопределение метода [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        String ln = System.lineSeparator();
+        return "{" + ln
+                + "\t\"name\" : " + "\"" + name + "\"" + "," + ln
+                + "\t\"body\" : " + "\"" + body + "\"" + ln
+                + "}";
+    }
+
+}
