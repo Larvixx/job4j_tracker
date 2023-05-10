b85677ae716c3c445550ffa1e8891234731202be
Andrey <weselov@list.ru>
2023-05-10 22:29:32 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Item {
     private LocalDateTime created = LocalDateTime.now();
@@ -13,6 +14,23 @@ public class Item {
 
     public Item(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id && Objects.equals(created, item.created) && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(created, id, name);
     }
 
     public Item(int id, String name) {
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,14 +1,65 @@
 package ru.job4j.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Tracker tracker = (Tracker) o;
+        return ids == tracker.ids && size == tracker.size && Arrays.equals(items, tracker.items);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(ids, size);
+        result = 31 * result + Arrays.hashCode(items);
+        return result;
+    }
+
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        int x = 0;
+        for (int index = 0; index < size; index++) {
+            Item name = items[index];
+            if (name != null) {
+                rsl[x] = name;
+                x++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, x);
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int x = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item name = items[index];
+            if (name != null && key.equals(name.getName())) {
+                rsl[x] = items[index];
+                x++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, x);
+        return rsl;
     }
 
     public Item findById(int id) {
