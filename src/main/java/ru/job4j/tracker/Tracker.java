b6cbb01791604b8286b54bb97d68f0452923a179
Andrey Weselov <weselov@list.ru>
2023-05-15 13:54:30 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -24,8 +24,8 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            System.arraycopy(items, index + 1, items
-                    , index, size - index - 1);
+            System.arraycopy(items, index + 1,
+                    items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
