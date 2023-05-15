767bd766473013ae80360a08f4681b190ec1ace9
Andrey Weselov <weselov@list.ru>
2023-05-15 15:19:36 +0300

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -27,7 +27,7 @@ public class Tracker {
             System.arraycopy(items, index + 1,
                     items, index, size - index - 1);
             items[size - 1] = null;
-            size--;
+            size --;
         }
         return rsl;
     }
