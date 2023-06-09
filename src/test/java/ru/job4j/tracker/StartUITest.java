105158e2c74aca139db1339b86079ce6506772a5
Larvixx <weselov@list.ru>
2023-06-09 10:41:16 +0300

    12.2. Тесты вывода на консоль [#33585  [#33585 #421742]]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class StartUITest {
+public class StartUITest {1
 
     @Test
     public void whenReplaceItemTestOutputIsSuccessfully() {
