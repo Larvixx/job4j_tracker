a5c71120f2f0ca8f2ad8024664853f5c299dbb0a
Larvixx <weselov@list.ru>
2023-06-22 21:09:35 +0300

    4. Принципы раннего возврата и охранных выражений [#504875  [#504875]]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -39,6 +39,9 @@ public class PasswordValidator {
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
             }
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
+            }
         }
         if (!hasUpCase) {
             throw new IllegalArgumentException(
