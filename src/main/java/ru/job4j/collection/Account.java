1b674d381241ff43aef3affef62bb7efb7f8f0e5
Larvixx <weselov@list.ru>
2023-10-12 20:03:15 +0300

    3. Модели данных и HashSet. [#218530]

--- src/main/java/ru/job4j/collection/Account.java
+++ src/main/java/ru/job4j/collection/Account.java
@@ -15,10 +15,14 @@ public class Account {
 
     @Override
     public String toString() {
-        return "Account{" +
-                "passport='" + passport + '\'' +
-                ", username='" + username + '\'' +
-                ", deposit='" + deposit + '\'' +
+        return "Account{"
+                +
+                "passport='" + passport + '\''
+                +
+                ", username='" + username + '\''
+                +
+                ", deposit='" + deposit + '\''
+                +
                 '}';
     }
 
