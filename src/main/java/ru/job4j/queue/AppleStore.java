d5a7f28a30106e8fc97aeb60dbb5773d9ebcead1
Larvixx <weselovv@list.ru>
2023-10-15 17:45:18 +0300

    Fix

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,22 @@
+package ru.job4j.queue;
+
+import java.util.Queue;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        return "";
+    }
+
+    public String getFirstUpsetCustomer() {
+        return "";
+    }
+}
