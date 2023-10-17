e9e34103a1cc121cd13325f3faeb9700be24fe0e
Larvixx <weselov@list.ru>
2023-10-17 14:47:42 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap <String, String> account = new HashMap<>();
+        account.put("weselov@list.ru", "Weselov Andrey Igorevich");
+        for (String key : account.keySet()) {
+            String value = account.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
