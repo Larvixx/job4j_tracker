60f98661e0fea7fcecfba5b0475ffb11239eb2fd
Larvixx <weselov@list.ru>
2023-10-19 19:46:09 +0300

    0. Map, HashMap [#212652 #453270]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> account = new HashMap<>();
         account.put("weselov@list.ru", "Weselov Andrey Igorevich");
+        account.put("weselov@list.ru", "Weselov  Denis Nikolaevich");
+        account.put("ivanov@list.ru", "Ivanov  Ivan Ivanovich");
         for (String key : account.keySet()) {
             String value = account.get(key);
             System.out.println(key + " = " + value);
