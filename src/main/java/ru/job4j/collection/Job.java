cb348724f2191f48c21b2bfdff7c0ed9df9793e1
Larvixx <weselov@list.ru>
2024-02-22 23:03:47 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/main/java/ru/job4j/collection/Job.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+public class Job {
+    private String name;
+
+    private int priority;
+
+    public Job(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" +
+                "name='" + name + '\'' +
+                ", priority=" + priority +
+                '}';
+    }
+
+}
---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenComparatorByNameAndPriority() {
+        Comparator<Job> cmpNamePriority =
+                new JobDescIncByName().thenComparing(new JobDescIncByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isEqualTo(3);
+    }
+
+    @Test
+    public void whenComparatorByIncPriority() {
+        int rsl = new JobDescIncByPriority().compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isEqualTo(-1);
+    }
+
+    @Test
+    public void whenComparatorByDecPriority() {
+        int rsl = new JobDescDecByPriority().compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isEqualTo(1);
+    }
+
+    @Test
+    public void whenComparatorByIncName() {
+        int rsl = new JobDescIncByName().compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isEqualTo(3);
+    }
+
+    @Test
+    public void whenComparatorByDecName() {
+        int rsl = new JobDescDecByName().compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isEqualTo(-3);
+    }
+}
