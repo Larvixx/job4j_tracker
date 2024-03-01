59d493203b1af91a26d2f3ad1469200fb2b90b16
Larvixx <weselov@list.ru>
2024-03-01 22:50:43 +0300

    5. PriorityQueue [#504944]

--- src/main/java/ru/job4j/queue/TaskByUrgencyDesc.java
+++ src/main/java/ru/job4j/queue/TaskByUrgencyDesc.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class TaskByUrgencyDesc implements Comparator<Task> {
     @Override
     public int compare(Task o1, Task o2) {
-        return Integer.compare(o2.urgency(),o1.urgency());
+        return Integer.compare(o2.urgency(), o1.urgency());
     }
 }
