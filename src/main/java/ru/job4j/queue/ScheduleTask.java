224d2e53cc6d3ebd76fcca991c7d79fafcf0989e
Larvixx <weselov@list.ru>
2024-03-01 22:47:59 +0300

    5. PriorityQueue [#504944]

---
+++ src/main/java/ru/job4j/queue/ScheduleTask.java
@@ -0,0 +1,25 @@
+package ru.job4j.queue;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class ScheduleTask {
+    private PriorityQueue<Task> queue;
+
+    public ScheduleTask(Comparator<Task> comparator) {
+        this.queue = new PriorityQueue<>(comparator);
+    }
+
+    public void addTask(Task task) {
+        queue.add(task);
+
+    }
+
+    public Task readTask() {
+        return queue.peek();
+    }
+
+    public Task getTask() {
+        return queue.poll();
+    }
+}
