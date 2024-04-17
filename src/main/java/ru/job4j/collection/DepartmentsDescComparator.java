53cc5ae161ab12be75ee5435ffb4e37ef658d196
Larvix <weselov@list.ru>
2024-04-18 00:26:31 +0300

    3. Отсортировать департаменты [#34352 #488011]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -1,10 +1,16 @@
 package ru.job4j.collection;
-
+1
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        return 0;
+        if (left.split("/", 2)[0].equals(right.split("/", 2)[0])) {
+            return left.compareTo(right);
+        }
+        return right.compareTo(left);
     }
 }
--- src/test/java/ru/job4j/collection/DepartmentsDescComparatorTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsDescComparatorTest.java
@@ -1,5 +1,5 @@
 package ru.job4j.collection;
-
+1
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 class DepartmentsDescComparatorTest {
