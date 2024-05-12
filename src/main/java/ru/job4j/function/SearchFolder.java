aad480b70b922085797dee4a481210122d3dd745
Larvix <weselov@list.ru>
2024-05-12 15:28:01 +0300

    2.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -0,0 +1,18 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFolder {
+
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> predicate) {
+        List<Folder> result = new ArrayList<>();
+        for (Folder folder : list) {
+            if (predicate.test(folder)) {
+                result.add(folder);
+            }
+        }
+        return result;
+    }
+}
